1d3deea0fc4b6ee03505a06745a712e7176af2b7
jack1228 <jack.favaron@gmail.com>
2020-06-05 11:16:05 +0200

    li pusho man mano

--- adapter/TestListAdapter.java
+++ adapter/TestListAdapter.java
@@ -18,7 +18,7 @@ public class TestListAdapter {
 	// Tests
 
 	@Test
-	public void TestAddWithParams() {
+	public void TestAddWithParamsFirstPosition() {
 		Object o = new Object();
 		l.add(0, o);
 		assertEquals(o, l.get(0));
@@ -33,6 +33,16 @@ public class TestListAdapter {
 	@Test (expected = IndexOutOfBoundsException.class)
 	public void testAddWithParamsIndexOutOfBounds() {
 		l.add(-1, new Object());
+	}
+
+	@Test
+	public void TestAddWithParamsLastPosition() {
+		for(int i = 0; i < 5; i++) {
+			l.add(new Object());
+		}
+		Object o = new Object();
+		l.add(l.size(), o);
+		assertEquals(o, l.get(l.size()-1));
 	}
 
 
