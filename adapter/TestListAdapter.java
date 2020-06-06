8794ae5a58aea9836655b1c1c4b59dd362153f5e
jack1228 <jack.favaron@gmail.com>
2020-06-06 10:07:19 +0200

    javadoc

--- adapter/TestListAdapter.java
+++ adapter/TestListAdapter.java
@@ -187,9 +187,131 @@ public class TestListAdapter {
 		assertTrue(l.contains(o));
 	}
 
+	@Test
+	public void testContainsObjectNotContained() {
+		for(int i = 0; i < 5; i++) {
+			l.add(new Object());
+		}
+		Object o = new Object();
+		assertFalse(l.contains(o));
+	}
+
 	@Test (expected = NullPointerException.class)
 	public void testContainsWithNullObject() {
 		l.contains(null);
 	}
+
+	/**
+     * TestContainsAll
+     */
+
+    @Test
+    public void testContainsAllWithHCollectionContained() {
+        HCollection c = new CollectionAdapter();
+        for(int i = 0; i < 5; i++) {
+            c.add(new Object());
+        }
+        l.addAll(c);
+        assertTrue(l.containsAll(c));
+    }
+
+    @Test
+    public void testContainsAllWithHCollectionNotContained() {
+        HCollection c = new CollectionAdapter();
+        for(int i = 0; i < 5; i++) {
+            c.add(new Object());
+        }
+        assertFalse(l.containsAll(c));
+	}
+	
+	@Test(expected = NullPointerException.class)
+    public void testContainsAllWithNull() {
+        l.containsAll(null);
+	}
+	
+	/**
+     * TestEquals
+     */
+
+    @Test
+    public void testEqualsTrue() {
+        HList otherList = new ListAdapter();
+        for(int i = 0; i < 5; i++) {
+			Object o = new Object();
+            otherList.add(o);
+            l.add(o);
+        }
+        assertTrue(l.equals(otherList));
+    }
+
+    @Test
+    public void testEqualsFalse() {
+		HList otherList = new ListAdapter();
+		otherList.add(new Object());
+		l.add(new Object());
+        assertFalse(l.equals(otherList));
+	}
+	
+	@Test
+	public void testEqualsWithNull() {
+		assertFalse(l.equals(null));
+	}
+
+	/**
+	 * Test get
+	 */
+
+	@Test
+    public void testGet() {
+		Object o = new Object();
+		l.add(o);
+        assertEquals(o, l.get(0));
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+    public void testGetOutOfBoundsNegative() {
+		l.add(new Object());
+        l.get(-1);
+	}
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testGetOutOfBoundsGreaterThanSize() {
+		for(int i = 0; i < 5; i++) {
+			l.add(new Object());
+		}
+		l.get(6);
+	}
+
+	/**
+     * TestIsEmpty
+     */
+
+    @Test
+    public void testIsEmptyTrue() {
+        assertTrue(l.isEmpty());
+	}
+	
+	@Test
+    public void testIsEmptyFalse() {
+		l.add(new Object());
+        assertFalse(l.isEmpty());
+	}
+	
+	/**
+     * TestIterator
+     */
+
+    @Test
+    public void testIterator() {
+        for(int i = 0; i < 5; i++) {
+            l.add(new Object());
+		}
+        HIterator it = l.iterator();
+        HList otherList = new ListAdapter();
+        while(it.hasNext()) {
+            otherList.add(it.next());
+        }
+        assertTrue(l.equals(otherList));
+    }
 
 }
